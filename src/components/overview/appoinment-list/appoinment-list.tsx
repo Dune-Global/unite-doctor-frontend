1d090c33af28d0436a16f99e2baf2734190dacff
WasathTheekshana <wasath.vt@gmail.com>
2024-03-17 23:38:22 +0530

    refactor: Appoinments finish

--- src/components/overview/appoinment-list/appoinment-list.tsx
+++ src/components/overview/appoinment-list/appoinment-list.tsx
@@ -15,29 +15,34 @@ export default function AppoinmentList() {
     return () => clearInterval(timer);
   }, []);
 
+  const ongoingOrUpcomingAppointments = AppointmentsApprove.filter((patient) => {
+    const endTime = new Date(`${patient.appoinmentDate} ${patient.appoinmentEndTime}`);
+    return currentTime < endTime;
+  });
+
   return (
     <div className="bg-ugray-0 mt-6 rounded-lg p-4">
       <div className="flex flex-col gap-7">
-        {AppointmentsApprove.map((patient) => {
-          const endTime = new Date(`${patient.appoinmentDate} ${patient.appoinmentEndTime}`);
-          if (currentTime < endTime) {
-            return (
-              <AppoinmentPatientCard
-                key={patient.id}
-                name={patient.name}
-                appoinmentNo={patient.appoinmentNo}
-                imageUrl={patient.imageUrl}
-                status={patient.status}
-                appoinmentDate={patient.appoinmentDate}
-                appoinmentEndTime={patient.appoinmentEndTime}
-                appoinmentStartTime={patient.appoinmentStartTime}
-                currentTime={currentTime}
-              />
-            );
-          } else {
-            return null;
-          }
-        })}
+        {ongoingOrUpcomingAppointments.length > 0 ? (
+          ongoingOrUpcomingAppointments.map((patient) => (
+            <AppoinmentPatientCard
+              key={patient.id}
+              name={patient.name}
+              appoinmentNo={patient.appoinmentNo}
+              imageUrl={patient.imageUrl}
+              status={patient.status}
+              appoinmentDate={patient.appoinmentDate}
+              appoinmentEndTime={patient.appoinmentEndTime}
+              appoinmentStartTime={patient.appoinmentStartTime}
+              currentTime={currentTime}
+            />
+          ))
+        ) : (
+          <p className='text-center text-xl font-medium'>No appointments</p>
+        )}
+      </div>
+      <div>
+          
       </div>
     </div>
   );
