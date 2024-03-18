4cdf074e35303daa50b24e4c145d616457bb68de
Lithara Perera <litharaperera2002@gmail.com>
2024-03-19 00:25:01 +0530

    refactor: Profile page tabs

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground",
+      "inline-flex h-10 items-center justify-center shadow-sm rounded-lg bg-ugray-0 text-muted-foreground",
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm",
+      "inline-flex items-center whitespace-nowrap justify-center focus:text-ublue-200 focus:border-b-2 py-3 text-sm transition-all focus:boder-b-2 disabled:pointer-events-none disabled:opacity-50",
       className
     )}
     {...props}
