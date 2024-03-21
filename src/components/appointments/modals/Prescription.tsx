491d4d40834d1a45893f349826815ce9eb8651df
Shenon69 <ask.tikki69@gmail.com>
2024-03-22 02:37:10 +0530

    feat: Prescription interface

--- src/components/appointments/modals/Prescription.tsx
+++ src/components/appointments/modals/Prescription.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 import {
     Dialog,
+    DialogClose,
     DialogContent,
     DialogHeader,
     DialogTitle,
@@ -22,33 +23,95 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button as UButton } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar";
 import { Input } from "@/components/ui/input"
 import { getCurrentDateTime } from '@/utils/getCurrentDateTime'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { appointmentStage, medicineTime } from '@/data/mock/appointment-prescription'
-import { CirclePlus } from 'lucide-react'
+import { CalendarIcon, CirclePlus } from 'lucide-react'
+import { format } from 'date-fns'
+import { cn } from '@/lib/utils'
 
 type Props = {
     cellContent: string
 }
 
 const formSchema = z.object({
-    firstName: z.string().min(2).max(50),
-    lastName: z.string().min(2).max(50),
-    age: z.string().min(1).max(3),
+    firstName: z.string({
+        required_error: "The first name should contain atleast 2 characters"
+    }).min(2, {
+        message: "The first name should contain atleast 2 characters"
+    }).max(50, {
+        message: "The first name should contain atmost 50 characters"
+    }),
+    lastName: z.string({
+        required_error: "The last name should contain atleast 2 characters"
+    }).min(2, {
+        message: "The last name should contain atleast 2 characters"
+
+    }).max(50, {
+        message: "The last name should contain atmost 50 characters"
+    }),
+    age: z.string({
+        required_error: "The age is required",
+    }).min(1, {
+        message: "The age should contain atleast 1 character"
+    }).max(3, {
+        message: "The age should contain atmost 3 characters "
+    }),
     gender: z.string().min(4).max(6),
-    allergies: z.string().min(2).max(150),
-    symptoms: z.string().min(2).max(150),
-    disease: z.string().min(2).max(150),
+    allergies: z.string().min(2, {
+        message: "The allergies should contain atleast 2 characters"
+    }).max(150, {
+        message: "The allergies should contain atmost 150 characters"
+
+    }),
+    symptoms: z.string().min(2, {
+        message: "The symptoms should contain atleast 2 characters"
+
+    }).max(150, {
+        message: "The symptoms should contain atmost 150 characters"
+
+    }),
+    disease: z.string().min(2, {
+        message: "The disease should contain atleast 2 characters"
+    }).max(150, {
+        message: "The disease should contain atmost 150 characters"
+    }),
     stage: z.string({
         required_error: "The stage is required",
     }),
-    medicineName: z.string().min(2).max(100),
-    dose: z.string().min(2).max(100),
+    medicineName: z.string({
+        required_error: "The medicine name is required",
+    }).min(0).max(100, {
+        message: "The medicine name should contain atmost 100 characters"
+    }),
+    dose: z.string({
+        required_error: "The dose is required",
+    }).min(2).max(255, {
+        message: "The dose should contain atmost 255 characters"
+    }),
     time: z.string({
         required_error: "The time is required",
     }),
-    bloodPressure: z.string().min(2).max(100),
+    bloodPressure: z.string().min(0).max(100),
+    nextChannelDate: z.date().optional(),
+    weight: z.string().min(1).max(100),
+    height: z.string().min(1).max(100),
+    reportname: z.string().min(0).max(200, {
+        message: "The report name should contain atmost 200 characters"
+    }),
+    dateToBeTaken: z.date().optional(),
+    other: z.string().min(0).max(255, {
+        message: "Only 255 characters are allowed for other field"
+
+    }),
 })
 
 export default function Prescription({
@@ -69,6 +132,10 @@ export default function Prescription({
             medicineName: "",
             bloodPressure: "",
             dose: "",
+            weight: "",
+            height: "",
+            reportname: "",
+            other: "",
         },
     })
 
@@ -303,9 +370,167 @@ export default function Prescription({
                                         )}
                                     />
 
+                                    <FormField
+                                        control={form.control}
+                                        name="nextChannelDate"
+                                        render={({ field }) => (
+                                            <FormItem className='w-72'>
+                                                <FormLabel>Next Channel Date</FormLabel>
+                                                <Popover>
+                                                    <PopoverTrigger asChild>
+                                                        <FormControl>
+                                                            <UButton
+                                                                variant={"outline"}
+                                                                className={cn(
+                                                                    "w-full border-ugray-100 pl-3 text-left font-normal py-[23px]",
+                                                                    !field.value && "text-muted-foreground"
+                                                                )}
+                                                            >
+                                                                {field.value ? (
+                                                                    format(field.value, "PPP")
+                                                                ) : (
+                                                                    <span className='text-ugray-400'>MM/DD/YYYY</span>
+                                                                )}
+                                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                            </UButton>
+                                                        </FormControl>
+                                                    </PopoverTrigger>
+                                                    <PopoverContent className="w-auto p-0" align="start">
+                                                        <Calendar
+                                                            mode="single"
+                                                            selected={field.value}
+                                                            onSelect={field.onChange}
+                                                            disabled={(date) =>
+                                                                date > new Date() ||
+                                                                date < new Date("1900-01-01")
+                                                            }
+                                                            initialFocus
+                                                        />
+                                                    </PopoverContent>
+                                                </Popover>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+
                                 </div>
 
-                                <Button type="submit" variant={"default"}>Submit</Button>
+                                <div className='flex flex-col md:flex-row justify-between gap-5'>
+                                    <FormField
+                                        control={form.control}
+                                        name="weight"
+                                        render={({ field }) => (
+                                            <FormItem className='w-72'>
+                                                <FormLabel>Weight</FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+
+                                    <FormField
+                                        control={form.control}
+                                        name="height"
+                                        render={({ field }) => (
+                                            <FormItem className='w-72'>
+                                                <FormLabel>Height</FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <div className='flex flex-col md:flex-row justify-between gap-5'>
+                                    <FormField
+                                        control={form.control}
+                                        name='reportname'
+                                        render={({ field }) => (
+                                            <FormItem className='w-72'>
+                                                <FormLabel className='flex flex-row gap-1 align-middle'>
+                                                    <div>Report Name</div>
+                                                    <button>
+                                                        <CirclePlus className='text-ugray-400' size={16} />
+                                                    </button>
+                                                </FormLabel>
+                                                <FormControl>
+                                                    <Input {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+
+                                    <FormField
+                                        control={form.control}
+                                        name="dateToBeTaken"
+                                        render={({ field }) => (
+                                            <FormItem className='w-72'>
+                                                <FormLabel>Date to be Taken</FormLabel>
+                                                <Popover>
+                                                    <PopoverTrigger asChild>
+                                                        <FormControl>
+                                                            <UButton
+                                                                variant={"outline"}
+                                                                className={cn(
+                                                                    "w-full border-ugray-100 pl-3 text-left font-normal py-[23px]",
+                                                                    !field.value && "text-muted-foreground"
+                                                                )}
+                                                            >
+                                                                {field.value ? (
+                                                                    format(field.value, "PPP")
+                                                                ) : (
+                                                                    <span className='text-ugray-400'>MM/DD/YYYY</span>
+                                                                )}
+                                                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                                            </UButton>
+                                                        </FormControl>
+                                                    </PopoverTrigger>
+                                                    <PopoverContent className="w-auto p-0" align="start">
+                                                        <Calendar
+                                                            mode="single"
+                                                            selected={field.value}
+                                                            onSelect={field.onChange}
+                                                            disabled={(date) =>
+                                                                date > new Date() ||
+                                                                date < new Date("1900-01-01")
+                                                            }
+                                                            initialFocus
+                                                        />
+                                                    </PopoverContent>
+                                                </Popover>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+
+                                <FormField
+                                    control={form.control}
+                                    name="other"
+                                    render={({ field }) => (
+                                        <FormItem className='w-72'>
+                                            <FormLabel>Other</FormLabel>
+                                            <FormControl>
+                                                <Input {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <div className='flex w-full justify-end gap-2 md:gap-3'>
+                                    <DialogClose asChild>
+                                        <Button type="button" variant="outline">
+                                            Cancel
+                                        </Button>
+                                    </DialogClose>
+                                    <Button type="submit" variant={"default"}>Submit</Button>
+                                </div>
                             </form>
                         </Form>
                     </div>
