4daaf53a4c4a72d34ba96133c0fc56027cd00d30
Shenon69 <ask.tikki69@gmail.com>
2024-03-22 00:37:59 +0530

    refactor: Date time funciton

--- src/utils/getCurrentDateTime.ts
+++ src/utils/getCurrentDateTime.ts
@@ -2,13 +2,13 @@ export const getCurrentDateTime = (): string => {
   const currentDate = new Date();
 
   const day = String(currentDate.getDate()).padStart(2, "0");
-  const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // Month starts from 0
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
   const year = currentDate.getFullYear();
 
   let hours = currentDate.getHours();
   const minutes = String(currentDate.getMinutes()).padStart(2, "0");
   const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12; // Convert 0 to 12 using logical OR operator
+  hours = hours % 12 || 12;
 
   const formattedDateTime = `${day}/${month}/${year} | ${hours}:${minutes} ${ampm}`;
 
