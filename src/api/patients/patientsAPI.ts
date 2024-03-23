928e5104d21c031de80961f92fe20063e8feeae6
Shenon69 <ask.tikki69@gmail.com>
2024-03-24 00:12:28 +0530

    feat: Patient list

---
+++ src/api/patients/patientsAPI.ts
@@ -0,0 +1,16 @@
+import { BACKEND_BASE_URL, CONNECTED_PATIENTS_URL } from "../_url/auth/url";
+import CustomAxios from "@/utils/axiosInstance";
+
+export const getConnectedPatients = async () => {
+  try {
+    const response = await CustomAxios({
+      method: "GET",
+      baseURL: BACKEND_BASE_URL,
+      url: CONNECTED_PATIENTS_URL,
+    });
+    console.log("\n\nresponse from pateintList", response);
+    return response;
+  } catch (error: any) {
+    return error.response;
+  }
+};
--- src/app/(client)/(pages)/dashboard/patients/page.tsx
+++ src/app/(client)/(pages)/dashboard/patients/page.tsx
@@ -1,23 +1,58 @@
 "use client"
 
-import { PatientList } from "@/types/patients"
+import { PatientData, PatientInfo } from "@/types/patients"
 import { columns } from "./(table)/columns"
 import { DataTable } from "./(table)/data-table"
 import { useEffect, useState } from "react"
-import { getPatientList } from "@/data/mock/patient-list"
+import { getConnectedPatientsActionHandler } from "@/actionLayer/patients/patientsAction"
+import { calculateAge } from "@/helpers/calculateAge"
 
 
 export default function Patient() {
-  const [data, setData] = useState<PatientList[]>([])
+  const [data, setData] = useState<PatientInfo[]>([])
 
   useEffect(() => {
-    const getPatientListActionHandler = async () => {
-      const data = await getPatientList();
-      setData(data)
+    const fetchPatientList = async () => {
+      const response = await getConnectedPatientsActionHandler()
+      const patientsList: PatientData[] = response.data
+
+      const transformedData: PatientData[] = patientsList.map(item => {
+        return {
+          sessionId: item.sessionId,
+          patient: {
+            _id: item.patient._id,
+            firstName: item.patient.firstName,
+            lastName: item.patient.lastName,
+            email: item.patient.email,
+            dateOfBirth: item.patient.dateOfBirth,
+            gender: item.patient.gender,
+            imgUrl: item.patient.imgUrl,
+            password: item.patient.password,
+            allergies: item.patient.allergies,
+            isEmailVerified: item.patient.isEmailVerified,
+            createdAt: item.patient.createdAt,
+            updatedAt: item.patient.updatedAt,
+            __v: item.patient.__v,
+          },
+          status: item.status
+        };
+      });
+
+      const patientTableObjects = transformedData.map(item => ({
+        patientName: `${item.patient.firstName} ${item.patient.lastName}`,
+        email: item.patient.email,
+        gender: item.patient.gender,
+        age: calculateAge(item.patient.dateOfBirth),
+        allergies: item.patient.allergies ?? "N/A",
+        imgUrl: item.patient.imgUrl
+      }));
+
+      setData(patientTableObjects)
     }
 
-    getPatientListActionHandler()
+    fetchPatientList()
   }, [])
+
 
   return (
     <>
