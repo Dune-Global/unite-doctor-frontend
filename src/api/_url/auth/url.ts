2ed1bed5a3975582aa5600a035b833385ede17d6
Shenon69 <ask.tikki69@gmail.com>
2024-03-24 19:12:28 +0530

    feat: Update appointment status

--- src/api/_url/auth/url.ts
+++ src/api/_url/auth/url.ts
@@ -8,5 +8,7 @@ export const ACCOUNT_ACTIVATION_URL = "/doctor/activate-account";
 export const CONNECTED_PATIENTS_URL = "/patient-doc/connected-patients";
 
 export const GET_UPCOMING_APPOINTMENTS_URL = "/appointment/doctor-appointments";
+export const UPDATE_APPOINTMENT_STATUS_URL =
+  "/appointment/doctor-appointment-status/:appointmentId";
 
 export const ADD_PRESCRIPTION_URL = "/patient-doc/add-prescription/:patientId";
